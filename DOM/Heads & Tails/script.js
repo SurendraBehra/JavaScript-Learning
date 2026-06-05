ae67b54c465b918bc0b0a5ed3beaa6518e8bb53b
surendra <surendrabehra2005@gmail.com>
2026-06-05 11:08:45 +0530

    remove unnecessary code

--- DOM/Heads & Tails/script.js
+++ DOM/Heads & Tails/script.js
@@ -8,8 +8,3 @@ function tails(){
   const Result = document.querySelector('.result');
   Result.innerHTML = `you choose tails`;
 } 
-function result(){
-  const inputElement = document.querySelector('.inputbtn');
-  document.querySelector('.js-para').innerHTML = inputElement.value;
-}
-
